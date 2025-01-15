3b1103b4100582d8a7217b7efb1a1e4112774434
Egor Ivanov <rust_lang_lover@proton.me>
2025-01-15 20:45:20 +0300

    // added custom_filters.py

---
+++ custom_filters.py
@@ -0,0 +1,37 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
+
+TOKEN = "6907074579:AAFJOtvMEDN8ewOVP4XnxOxWyZY-OTjLXXM"
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+admin_ids: list[int] = [173901673]
+
+
+# Собственный фильтр, проверяющий юзера на админа
+class IsAdmin(BaseFilter):
+    def __init__(self, admin_ids: list[int]) -> None:
+        # В качестве параметра фильтр принимает список с целыми числами
+        self.admin_ids = admin_ids
+
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id in self.admin_ids
+
+
+# Этот хэндлер будет срабатывать, если апдейт от админа
+@dp.message(IsAdmin(admin_ids))
+async def answer_if_admins_update(message: Message):
+    await message.answer(text='Вы админ')
+
+
+# Этот хэндлер будет срабатывать, если апдейт не от админа
+@dp.message()
+async def answer_if_not_admins_update(message: Message):
+    await message.answer(text='Вы не админ')
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
