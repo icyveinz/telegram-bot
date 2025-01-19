7d191af36bb68bd9f6133a200df1ea0f1f0db027
Egor Ivanov <rust_lang_lover@proton.me>
2025-01-19 18:21:21 +0300

    // updated from_filter_to_handler.py

--- from_filter_to_handler.py
+++ from_filter_to_handler.py
@@ -1,35 +1,35 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
+
 
 TOKEN = "6907074579:AAFJOtvMEDN8ewOVP4XnxOxWyZY-OTjLXXM"
 
 # Создаем объекты бота и диспетчера
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
-admin_ids: list[int] = [173901673]
+
+class MyCustomFilter(BaseFilter):
+    async def __call__(self, message: Message) -> bool | dict[str, list[int]]:
+        numbers = []
+        for word in message.text.split():
+            normalized_word = word.replace('.', '').replace(',', '').strip()
+            if normalized_word.isdigit():
+                numbers.append(int(normalized_word))
+            # Если в списке есть числа - возвращаем словарь со списком чисел по ключу 'numbers'
+        if numbers:
+            return {'numbers': numbers}
+        return False
 
 
-# Собственный фильтр, проверяющий юзера на админа
-class IsAdmin(BaseFilter):
-    def __init__(self, admin_ids: list[int]) -> None:
-        # В качестве параметра фильтр принимает список с целыми числами
-        self.admin_ids = admin_ids
-
-    async def __call__(self, message: Message) -> bool:
-        return message.from_user.id in self.admin_ids
+@dp.message(F.text.lower().startswith('найди'), MyCustomFilter())
+async def process_if_no_numbers(message : Message, numbers : list[int]):
+    await message.answer(text=f"Я нашел числа : {', '.join(str(num) for num in numbers)}")
 
 
-# Этот хэндлер будет срабатывать, если апдейт от админа
-@dp.message(IsAdmin(admin_ids))
-async def answer_if_admins_update(message: Message):
-    await message.answer(text='Вы админ')
-
-
-# Этот хэндлер будет срабатывать, если апдейт не от админа
-@dp.message()
-async def answer_if_not_admins_update(message: Message):
-    await message.answer(text='Вы не админ')
+@dp.message(F.text.lower().startswith('найди'))
+async def process_if_no_numbers(message : Message):
+    await message.answer(text="Я не нашел чисел внутри сообщения")
 
 
 if __name__ == '__main__':
