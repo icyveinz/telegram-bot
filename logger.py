2d1e778921033c25f03b8f94bfa4ca7334e4282a
Egor Ivanov <rust_lang_lover@proton.me>
2025-01-20 19:41:53 +0300

    //

--- logger.py
+++ logger.py
@@ -1,9 +1,19 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] #%(levelname)-8s %(filename)s:'
+           '%(lineno)d - %(name)s - %(message)s'
+)
 
 logging.debug('Это лог уровня DEBUG')
 logging.info('Это лог уровня INFO')
 logging.warning('Это лог уровня WARNING')
 logging.error('Это лог уровня ERROR')
 logging.critical('Это лог уровня CRITICAL')
+
+try:
+    print(4 / 2)
+    print(2 / 0)
+except ZeroDivisionError:
+    logging.error('Тут было исключение', exc_info=True)
