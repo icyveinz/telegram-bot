f2c2f291dcdffc86e776ca0c4b8bc3abbe1f8d04
Egor Ivanov <rust_lang_lover@proton.me>
2025-01-27 20:30:10 +0300

    //

--- menu_example.py
+++ menu_example.py
@@ -1,11 +1,12 @@
-import random
 from aiogram import Bot, Dispatcher, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, Message, KeyboardButtonPollType, \
-    BotCommand
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types import Message, BotCommand
+
+LEXICON_RU: dict[str, str] = {
+    '/start': 'Это пример бота, в котором есть меню с пунктами для отображения пользователю',
+    '/help': 'Заходи в репозиторий на ГИТЕ https://github.com/icyveinz/telegram-bot',
+}
 
 TOKEN = "6907074579:AAFJOtvMEDN8ewOVP4XnxOxWyZY-OTjLXXM"
 
@@ -32,6 +33,11 @@ async def show_help(message: Message):
     await message.reply(text=LEXICON_RU['/help'])
 
 
+@dp.message(F.text.lower().in_(["/start"]))
+async def show_help(message: Message):
+    await message.reply(text=LEXICON_RU['/start'])
+
+
 
 
 if __name__ == '__main__':
@@ -39,29 +45,3 @@ if __name__ == '__main__':
     # которая будет выполняться на старте бота,
     dp.startup.register(set_main_menu)
     dp.run_polling(bot)
-
-
-LEXICON_RU: dict[str, str] = {
-    '/start': '<b>Привет!</b>\nДавай с тобой сыграем в игру '
-              '"Камень, ножницы, бумага"?\n\nЕсли ты, вдруг, '
-              'забыл правила, команда /help тебе поможет!\n\n<b>Играем?</b>',
-    '/help': 'Это очень простая игра. Мы одновременно должны '
-             'сделать выбор одного из трех предметов. Камень, '
-             'ножницы или бумага.\n\nЕсли наш выбор '
-             'совпадает - ничья, а в остальных случаях камень '
-             'побеждает ножницы, ножницы побеждают бумагу, '
-             'а бумага побеждает камень.\n\n<b>Играем?</b>',
-    'rock': 'Камень 🗿',
-    'paper': 'Бумага 📜',
-    'scissors': 'Ножницы ✂',
-    'yes_button': 'Давай!',
-    'no_button': 'Не хочу!',
-    'other_answer': 'Извини, увы, это сообщение мне непонятно...',
-    'yes': 'Отлично! Делай свой выбор!',
-    'no': 'Жаль...\nЕсли захочешь сыграть, просто разверни '
-          'клавиатуру и нажми кнопку "Давай!"',
-    'bot_won': 'Я победил!\n\nСыграем еще?',
-    'user_won': 'Ты победил! Поздравляю!\n\nДавай сыграем еще?',
-    'nobody_won': 'Ничья!\n\nПродолжим?',
-    'bot_choice': 'Мой выбор'
-}
