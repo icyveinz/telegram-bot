4589c92a4771f7dc37f0496051b3399121d4b3a5
Egor Ivanov <rust_lang_lover@proton.me>
2025-01-28 19:36:12 +0300

    //

---
+++ inline_buttons.py
@@ -0,0 +1,46 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message, BotCommand, InlineKeyboardButton, InlineKeyboardMarkup
+
+LEXICON_RU: dict[str, str] = {
+    '/start': 'Это пример бота, в котором есть меню с пунктами для отображения пользователю',
+    '/help': 'Заходи в репозиторий на ГИТЕ https://github.com/icyveinz/telegram-bot',
+}
+
+TOKEN = "6907074579:AAFJOtvMEDN8ewOVP4XnxOxWyZY-OTjLXXM"
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
+
+# Создаем объекты инлайн-кнопок
+url_button_1 = InlineKeyboardButton(
+    text='Курс "Телеграм-боты на Python и AIOgram"',
+    url='https://null.org/120924'
+)
+url_button_2 = InlineKeyboardButton(
+    text='Документация Telegram Bot API',
+    url='https://core.telegram.org/bots/api'
+)
+
+# Создаем объект инлайн-клавиатуры
+keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[[url_button_1],
+                     [url_button_2]]
+)
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+# и отправлять в чат клавиатуру c url-кнопками
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(
+        text='Это инлайн-кнопки с параметром "url"',
+        reply_markup=keyboard
+    )
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
