85d775f4da6ca12781ed6e2625e8ee4fed476925
Egor Ivanov <rust_lang_lover@proton.me>
2025-01-30 20:35:23 +0300

    //

---
+++ callback_factory.py
@@ -0,0 +1,57 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+
+TOKEN = "6907074579:AAFJOtvMEDN8ewOVP4XnxOxWyZY-OTjLXXM"
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
+
+# Создаем свой класс фабрики коллбэков, указывая префикс
+# и структуру callback_data
+class GoodsCallbackFactory(CallbackData, prefix="goods"):
+    category_id: int
+    subcategory_id: int
+    item_id: int
+
+# Создаем объекты кнопок, с применением фабрики коллбэков
+button_1 = InlineKeyboardButton(
+    text='Категория 1',
+    callback_data=GoodsCallbackFactory(
+        category_id=1,
+        subcategory_id=0,
+        item_id=0
+    ).pack()
+)
+
+button_2 = InlineKeyboardButton(
+    text='Категория 2',
+    callback_data=GoodsCallbackFactory(
+        category_id=2,
+        subcategory_id=0,
+        item_id=0
+    ).pack()
+)
+
+# Создаем объект клавиатуры, добавляя в список списки с кнопками
+markup = InlineKeyboardMarkup(
+    inline_keyboard=[[button_1], [button_2]]
+)
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+# и отправлять в чат клавиатуру с инлайн-кнопками
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(
+        text='Вот клавиатура',
+        reply_markup=markup
+)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
